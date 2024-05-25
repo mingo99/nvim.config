fb1e914a6f3acecc0f1c905d624069719e0ff903
mingo99 <2839131656@qq.com>
2024-05-25 21:30:41 +0800

    install undotree, neorg and update

---
+++ lua/mingo/plugins/undotree.lua
@@ -0,0 +1,47 @@
+-- Visualize your undo tree and fuzzy-search changes in it.
+-- url: https://github.com/debugloop/telescope-undo.nvim
+return {
+	{
+		"debugloop/telescope-undo.nvim",
+		dependencies = { -- note how they're inverted to above example
+			{
+				"nvim-telescope/telescope.nvim",
+				dependencies = { "nvim-lua/plenary.nvim" },
+			},
+		},
+		keys = {
+			{ -- lazy style key map
+				"<leader>uh",
+				"<cmd>Telescope undo<cr>",
+				desc = "undo history",
+			},
+		},
+		opts = {
+			-- don't use `defaults = { }` here, do this in the main telescope spec
+			extensions = {
+				undo = {
+					-- telescope-undo.nvim config, see below
+					use_delta = true,
+					use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
+					side_by_side = true,
+					diff_context_lines = vim.o.scrolloff,
+					entry_format = "state #$ID, $STAT, $TIME",
+					time_format = "",
+					saved_only = false,
+					-- layout_strategy = "vertical",
+					-- layout_config = {
+					-- 	preview_height = 0.6,
+					-- },
+				},
+				-- no other extensions here, they can have their own spec too
+			},
+		},
+		config = function(_, opts)
+			-- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+			-- configs for us. We won't use data, as everything is in it's own namespace (telescope
+			-- defaults, as well as each extension).
+			require("telescope").setup(opts)
+			require("telescope").load_extension("undo")
+		end,
+	},
+}
